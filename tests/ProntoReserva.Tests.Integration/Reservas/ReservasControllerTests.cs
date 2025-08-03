d632be232f2ff4b465e6fb7ab88d7ff9ffc71be5
Herick2D <contato.herickmoreira@gmail.com>
2025-08-03 02:32:41 -0300

    FIX: Implementação de autorização para os testes unitários

--- tests/ProntoReserva.Tests.Integration/Reservas/ReservasControllerTests.cs
+++ tests/ProntoReserva.Tests.Integration/Reservas/ReservasControllerTests.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using ProntoReserva.Application.Features.Reservas.Commands.CreateReserva;
 using ProntoReserva.Application.Features.Reservas.Commands.UpdateReserva;
 using ProntoReserva.Application.Features.Reservas.Common;
+using ProntoReserva.Application.Features.Users.Commands.Login;
+using ProntoReserva.Application.Features.Users.Commands.Register;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 
@@ -16,6 +19,21 @@ public class ReservasControllerTests : IClassFixture<WebApplicationFactory<Progr
     public ReservasControllerTests(WebApplicationFactory<Program> factory)
     {
         _client = factory.CreateClient();
+    }
+    
+    private async Task AuthenticateClientAsync()
+    {
+        var email = $"teste-{Guid.NewGuid()}@testes.com";
+        var password = "senhavalida123!";
+        var registerCommand = new RegisterUserCommand(email, password);
+        await _client.PostAsJsonAsync("/api/auth/register", registerCommand);
+
+        var loginCommand = new LoginUserCommand(email, password);
+        var loginResponse = await _client.PostAsJsonAsync("/api/auth/login", loginCommand);
+        loginResponse.EnsureSuccessStatusCode();
+        var loginResult = await loginResponse.Content.ReadFromJsonAsync<LoginUserResponse>();
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", loginResult.Token);
     }
 
     private async Task<Guid> CriarReservaParaTeste(string nomeCliente)
@@ -30,6 +48,7 @@ public class ReservasControllerTests : IClassFixture<WebApplicationFactory<Progr
     [Fact]
     public async Task PostReserva_ComDadosValidos_DeveRetornarCreated()
     {
+        await AuthenticateClientAsync();
         var command = new CreateReservaCommand("Cliente de Integração", DateTime.UtcNow.AddDays(10), 4, "Teste de integração");
 
         var response = await _client.PostAsJsonAsync("/api/reservas", command);
@@ -41,14 +60,18 @@ public class ReservasControllerTests : IClassFixture<WebApplicationFactory<Progr
     [Fact]
     public async Task PostReserva_ComDadosInvalidos_DeveRetornarBadRequest()
     {
+        await AuthenticateClientAsync();
         var command = new CreateReservaCommand("Cliente Inválido", DateTime.UtcNow.AddDays(-1), 2, null);
+
         var response = await _client.PostAsJsonAsync("/api/reservas", command);
+
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
 
     [Fact]
     public async Task GetReservaById_QuandoReservaExiste_DeveRetornarOkComDadosCorretos()
     {
+        await AuthenticateClientAsync();
         var newId = await CriarReservaParaTeste("Cliente para Buscar");
 
         var getResponse = await _client.GetAsync($"/api/reservas/{newId}");
@@ -61,6 +84,7 @@ public class ReservasControllerTests : IClassFixture<WebApplicationFactory<Progr
     [Fact]
     public async Task GetAllReservas_QuandoExistemReservas_DeveRetornarOkComListaPaginada()
     {
+        await AuthenticateClientAsync();
         await CriarReservaParaTeste("Cliente na Lista");
 
         var response = await _client.GetAsync("/api/reservas?pageNumber=1&pageSize=10");
@@ -73,6 +97,7 @@ public class ReservasControllerTests : IClassFixture<WebApplicationFactory<Progr
     [Fact]
     public async Task UpdateReserva_QuandoReservaExiste_DeveRetornarNoContent()
     {
+        await AuthenticateClientAsync();
         var idParaAtualizar = await CriarReservaParaTeste("Cliente Antigo");
         var command = new UpdateReservaCommand(idParaAtualizar, "Cliente Novo", DateTime.UtcNow.AddDays(5), 5, "Atualizado");
 
@@ -88,6 +113,7 @@ public class ReservasControllerTests : IClassFixture<WebApplicationFactory<Progr
     [Fact]
     public async Task DeleteReserva_QuandoReservaExiste_DeveRetornarNoContent()
     {
+        await AuthenticateClientAsync();
         var idParaApagar = await CriarReservaParaTeste("Cliente a ser Apagado");
 
         var response = await _client.DeleteAsync($"/api/reservas/{idParaApagar}");
